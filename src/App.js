15c7fcc9923d576bd3f1c3bf0792a750a9fda7b4
Ömer Can Baltacı <152120131103@ogrenci.ogu.edu.tr>
2021-03-12 15:18:16 +0300

    Sign in and sign up structures

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
-import './App.css';
-import NavBar from './components/NavBar'
-import Stories from './components/Stories'
-import Flashcards from './components/Flashcards'
-import Quizzes from './components/Quizzes'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Stories from "./components/Stories";
+import Flashcards from "./components/Flashcards";
+import Quizzes from "./components/Quizzes";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Dashboard from "./components/Dashboard";
+import { AuthProvider } from "./contexts/AuthContext";
+import React from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import ForgotPassword from "./components/ForgotPassword";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/stories" component={Stories}>
-              <Stories />
-            </Route>
-            <Route path="/flashcards" component={Flashcards}>
-              <Flashcards />
-            </Route>
-            <Route path="/quizzes" component={Quizzes}>
-              <Quizzes />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <AuthProvider>
+        <NavBar />
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/stories" component={Stories} />
+              <Route path="/flashcards" component={Flashcards} />
+              <Route path="/quizzes" component={Quizzes} />
+            </Switch>
+          </div>
+          <Container
+            className="d-flex align-items-center justify-content-center"
+            style={{ minHeight: "100vh" }}
+          >
+            <div className="w-100" style={{ maxWidth: "400px" }}>
+              <AuthProvider>
+                <Switch>
+                  <Route path="/signup" component={SignUp} />
+                  <Route path="/signin" component={SignIn} />
+                  <Route path="/forgot-password" component={ForgotPassword} />
+                  <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                </Switch>
+              </AuthProvider>
+            </div>
+          </Container>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,31 @@
-import '../App.css';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import "../App.css";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import React from "react";
+import { useAuth } from "../contexts/AuthContext";
 
 const NavBar = () => {
+  const { currentUser } = useAuth();
+
   return (
-    <div className="App">
-      <Navbar collapseOnSelect="sm" bg="light" expand="lg">
-        <Navbar.Brand href="home">Language Learning App</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="signin">Sign In</Nav.Link>
-            <Nav.Link href="signup">Sign Up</Nav.Link>
-            <NavDropdown title="Activities" id="basic-nav-dropdown">
-              <NavDropdown.Item href="stories">Stories</NavDropdown.Item>
-              <NavDropdown.Item href="flashcards">Flashcards</NavDropdown.Item>
-              <NavDropdown.Item href="quizzes">Quizzes</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+    <Navbar collapseOnSelect="sm" bg="light" expand="lg">
+      <Navbar.Brand href="home">Language Learning App</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="signin">Sign In</Nav.Link>
+          <Nav.Link href="signup">Sign Up</Nav.Link>
+          <NavDropdown title="Activities" id="basic-nav-dropdown">
+            <NavDropdown.Item href="stories">Stories</NavDropdown.Item>
+            <NavDropdown.Item href="flashcards">Flashcards</NavDropdown.Item>
+            <NavDropdown.Item href="quizzes">Quizzes</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
       </Navbar.Collapse>
-      </Navbar>
-    </div>
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>Signed in as: asd</Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
   );
-}
+};
 
 export default NavBar;
